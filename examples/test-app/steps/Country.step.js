b1f22a2ca1b58f8aaaf60f042352d2f8629720f5
Michael Allen <michael@michaelallen.io>
2017-11-30 15:43:50 +0000

    Better country content

--- examples/test-app/steps/Country.step.js
+++ examples/test-app/steps/Country.step.js
@@ -1,6 +1,25 @@
 const { Question, goTo, branch } = require('@hmcts/one-per-page');
 const { form, textField } = require('@hmcts/one-per-page/forms');
+const { answer } = require('@hmcts/one-per-page/checkYourAnswers');
 const Joi = require('joi');
+
+
+const titleise = string => {
+  if (typeof string === 'undefined') {
+    return '';
+  }
+  if (string.length < 1) {
+    return string.toUpperCase();
+  }
+  const firstChar = string[0].toUpperCase();
+  const rest = string.slice(1)
+    .replace(/([A-Z])/g, ' $1')
+    .trim()
+    .toLowerCase();
+
+  return `${firstChar}${rest}`;
+};
+
 
 class Country extends Question {
   get form() {
@@ -22,6 +41,10 @@ class Country extends Question {
       goTo(this.journey.steps.ExitNorthernIreland).if(livesInNI),
       goTo(this.journey.steps.RespondentTitle)
     );
+  }
+
+  answers() {
+    return answer(this, { answer: titleise(this.fields.country.value) });
   }
 }
 
