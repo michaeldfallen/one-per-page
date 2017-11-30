417a5e749a4329b2bb63bb0b3195028bcc90c929
Michael Allen <michael@michaelallen.io>
2017-11-30 15:44:34 +0000

    Better name step content

--- examples/test-app/steps/Name.step.js
+++ examples/test-app/steps/Name.step.js
@@ -1,5 +1,6 @@
 const { Question, goTo } = require('@hmcts/one-per-page');
 const { form, textField } = require('@hmcts/one-per-page/forms');
+const { answer } = require('@hmcts/one-per-page/checkYourAnswers');
 const Joi = require('joi');
 
 class Name extends Question {
@@ -30,6 +31,26 @@ class Name extends Question {
       return this.fields.husbandOrWife.value;
     }
     return 'husband/wife';
+  }
+
+  answers() {
+    return [
+      answer(this, {
+        question: this.content.cya.petitioner.question,
+        answer: `${this.fields.firstName.value} ${this.fields.lastName.value}`,
+        value: {
+          firstName: this.fields.firstName.value,
+          lastName: this.fields.lastName.value
+        }
+      }),
+      answer(this, {
+        question: this.content.cya.respondent.question,
+        value: {
+          firstName: this.fields.respondentFirstName.value,
+          lastName: this.fields.respondentLastName.value
+        }
+      })
+    ];
   }
 
   next() {
